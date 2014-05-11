18690579f76397f5fdafcb91ee4e4c41ec41cd33
skorzewska <skorzewska.m@gmail.com>
2014-05-11 22:17:09 +0200

    added game over

--- board/display.py
+++ board/display.py
@@ -16,23 +16,27 @@ from mine import (
 )
 from random import sample, randint
 import pygame
+from copy import deepcopy
 
 
 class Display(object):
     def __init__(self, quantity):
         self.quantity = quantity
         self.menu_height = 50
+        self.gameover_height = 50
         self.width = self.size_by_name('width')
         self.height = self.size_by_name('height') + self.menu_height
         self.grid = [[0 for r in xrange(self.quantity)] for c in xrange(self.quantity)]
 
         self.title = "Miner"
         self.saper = Saper(quantity, "player")  # use here your bot name
+        self.lifes = 3          # put here number of lifes for miner
         self.done = False
 
         self.schemes = [Scheme(no, mine) for no, mine in
                         zip(sample(SCHEMES, 3), [GreenMine, YellowMine, RedMine])]
         self.place_mines()
+        self.grid_copy = deepcopy(self.grid)
 
         self.initialize_pygame()
 
@@ -48,7 +52,7 @@ class Display(object):
         self.clock = pygame.time.Clock()
 
         pygame.font.init()
-        self.font = pygame.font.SysFont("comicsansms", max(self.quantity * 2/3, 10))
+        self.font = pygame.font.SysFont("comicsansms", max(self.quantity * 2/3, 15))
 
     def place_mines(self):
         for scheme in self.schemes:
@@ -81,6 +85,11 @@ class Display(object):
         name = self.font.render("Name: {}".format(self.saper.name), True, BLACK)
         self.screen.blit(name, (self.width/50, 1))
 
+        current_lifes = self.font.render(
+            "Lifes: {}".format(self.lifes), True, BLACK
+            )
+        self.screen.blit(current_lifes, (self.width/2, 35))
+
         current_health = self.font.render(
             "Health: {}".format(self.saper.health), True, RED
             )
@@ -94,11 +103,18 @@ class Display(object):
             self.screen.blit(meter, (wpos, 13))
             wpos += self.width/7
 
+    def draw_gameover(self):
+        gameover_rect = pygame.Rect(0, 0, self.width, self.gameover_height)
+        pygame.draw.rect(self.screen, GREY, gameover_rect)
+
+        name = self.font.render("GAME OVER".format(), True, RED)
+        self.screen.blit(name, (self.width/50, 1))
+
     def compute_readout(self):
         x, y = self.saper.cords
 
     def run(self):
-        while not self.done:
+        while not self.done and self.lifes > 0:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.done = True
@@ -114,12 +130,20 @@ class Display(object):
 
                     row, column = self.saper.cords
                     self.saper.health -= self.grid[row][column].damage
-
+                    if self.saper.health <= 0:
+                        self.lifes -= 1
+                        self.saper.health = 100
+                        self.saper.reset_position()
+                        self.grid_copy = deepcopy(self.grid)
+                    
             self.screen.fill(BLACK)
 
             # draw here
             self.draw_grid()
-            self.draw_menu()
+            if self.lifes > 0:
+                self.draw_menu()
+            else:
+                self.draw_gameover()
 
             pygame.display.flip()
 
@@ -151,3 +175,6 @@ class Saper(object):
     def down(self):
         if self.cords[0] < self.grid_quan:
             self.cords[0] += 1
+
+    def reset_position(self):
+        self.cords = [self.grid_quan, self.grid_quan]
